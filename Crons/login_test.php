fdc999d0dc8bcf443b6ef12e0491e323cfbb159b
Cory <cory@kreatednine.com>
2014-12-03 15:54:06 -0500

    Added server side authentication

---
+++ Crons/login_test.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Using the Client Settings login using the credentials and retreive a session ID
+ */
+ 
+include realpath(dirname(__FILE__) . '/../Config/settings.php');
+
+
+$url = 'https://test.salesforce.com/services/oauth2/token';
+
+$post_data = array(
+	'grant_type'    => 'password',
+	'client_id'     => $clientSetting['clientId'],
+	'client_secret' => $clientSetting['secret'],
+	'username'      => $clientSetting['username'],
+	'password'      => $clientSetting['password'] . $clientSetting['token']
+);
+$post_data = http_build_query($post_data);
+echo $post_data;
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL,            $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST,           true);
+curl_setopt($ch, CURLOPT_POSTFIELDS,     $post_data); 
+curl_setopt($ch, CURLOPT_HTTPHEADER,     array('Content-Type: application/x-www-form-urlencoded')); 
+
+$result=curl_exec ($ch);
+
+$response = json_decode($result);
+echo "\n\n";
+
+var_dump($response);
+
+
+
+// Using the access token example
+$url = $response->instance_url . '/services/data/v20.0/sobjects/Account/';
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,            $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER,     array('Authorization: Bearer '.$response->access_token));
+$result = curl_exec($ch);
+$response = json_decode($result);
+echo "\n\n";
+var_dump($response);
